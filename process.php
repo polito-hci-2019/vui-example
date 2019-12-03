b801e85f14eb60b94bfe148c0c8db608d5cedaec
Luigi De Russis <luigidr@gmail.com>
2019-12-03 21:12:44 +0100

    code developed in class (still not working)

--- process.php
+++ process.php
@@ -1,10 +1,43 @@
 <?php
 
+use Google\Cloud\Dialogflow\V2\SessionsClient;
+use Google\Cloud\Dialogflow\V2\QueryInput;
+use Google\Cloud\Dialogflow\V2\TextInput;
+
+require __DIR__.'vendor/autoload.php';
+
 if(isset($_POST['submit'])) {
     $userquery = $_POST['message'];
 
-    if(preg_match('/hi|hello/', $userquery)==1)
-        echo "hello";
-    elseif(preg_match('/weather/', $userquery)==1)
-        echo "it will be sunny";
+    // create a new session
+    $credential = array('credentials' => "client-secret.json");
+    $sessionClient = new SessionsClient($credential);
+
+    try {
+        // replace the project id, since it is "fake"
+        $session = $sessionClient->sessionName("weatheragent-ysfsew", 123456 ?: uniqid());
+
+        // prepare a new query input...
+        $queryInput = new QueryInput();
+        // ... and the inner text
+        $textInput = new TextInput();
+        $textInput->setText($userquery);
+        $textInput->setLanguageCode("en-US");
+
+        $queryInput->setText($textInput);
+
+        // send the request and get the response
+        $response = $sessionClient->detectIntent($session, $queryInput);
+
+        $queryResult = $response->getQueryResult();
+
+        // the textual response is in "fullfilment"
+        $fulfillmentText = $queryResult->getFulfillmentText();
+
+        // finally, respond to the ajax request
+        echo $fulfillmentText;
+
+    } finally {
+        $sessionClient->close();
+    }
 }
